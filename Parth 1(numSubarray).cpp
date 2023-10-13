250739147f9b845574fee5f4b14866ea0cad9d0b
Parth-88 <112095322+Parth-88@users.noreply.github.com>
2023-10-13 21:27:09 +0530

    Create Parth 1(numSubarray).cpp

---
+++ Parth 1(numSubarray).cpp
@@ -0,0 +1,10 @@
+ int numSubarraysWithSum(vector<int>& A, int S) {
+        unordered_map<int, int> c({{0, 1}});
+        int psum = 0, res = 0;
+        for (int i : A) {
+            psum += i;
+            res += c[psum - S];
+            c[psum]++;
+        }
+        return res;
+    }
